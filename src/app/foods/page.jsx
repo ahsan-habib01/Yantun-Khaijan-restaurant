e1b25b0871f0cff71f966c389c1ba1e51915a04c
ahsan.habib01 <ahshapon00@gmail.com>
2025-12-29 22:19:29 +0600

    added dynamic title

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -14,6 +14,12 @@ const getFoods = async (search) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return data.foods || [];
 };
+
+export const metadata = {
+  title: 'All Foods - Yantun Khaijan',
+  description: 'Best fastfood restaurant in Noakhali',
+};
+
 const FoodsPage = async ({ searchParams }) => {
   const { search = "" } = await searchParams;
 
