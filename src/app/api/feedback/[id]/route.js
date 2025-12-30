f8106d40719b9eb5e9c269919812f7a92bb2ee56
ahsan.habib01 <ahshapon00@gmail.com>
2025-12-30 12:11:26 +0600

    get and post api created

---
+++ src/app/api/feedback/[id]/route.js
@@ -0,0 +1,14 @@
+import { feedback } from "../../route";
+
+export async function GET(request, { params }) {
+  const { id } = await params;
+
+  const singleFeedback = feedback.find(fb => fb.id == id);
+
+  if (!singleFeedback) {    
+    return Response.json({ error: 'Feedback not found' });
+  }         
+
+  return Response.json(singleFeedback)
+
+}
